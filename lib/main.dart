d44695181393a5a98309048b797857bd3c7242bd
1manar12 <manarkchaich@gmail.com>
2022-03-20 17:08:02 +0100

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter TEst',
+      title: 'Flutter test2',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
